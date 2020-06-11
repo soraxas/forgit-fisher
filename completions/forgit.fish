6cd50cfbabb762a38c00c19c4ebc4df79dcae6a7
Tin Lai <oscar@tinyiu.com>
2020-06-11 16:22:10 +1000

    cleanup

--- completions/forgit.fish
+++ completions/forgit.fish
@@ -1,7 +1,9 @@
+#!/usr/bin/env fish
 
 # no filename completion
 complete -c forgit -x
 
 for completions_cmd in (forgit --complete)
+    # perform completions with forgit builtin command
     eval "complete -c forgit $completions_cmd"
 end
--- completions/git-forgit.fish
+++ completions/git-forgit.fish
@@ -1,29 +1,12 @@
-if test -n "$__FISH_FORGIT_ALIAS_NAME"
+#!/usr/bin/env fish
 
+if test -n "$__FISH_FORGIT_ALIAS_NAME"
     # no filename completion
     complete -c git -xa "$__FISH_FORGIT_ALIAS_NAME" -d "Using fzf to empower your git workflow"
 
-    # add git prefix for the completion command
+    # add git prefix for the completion command, for `git forgit` alias
     for completions_cmd in (forgit --complete | sed 's/-n "/-n "__fish_git_using_command '$__FISH_FORGIT_ALIAS_NAME'; /')
         notify-send "complete -c git $completions_cmd"
     end
 
 end 
-# set -l __forgit_commands_first_tokens_only (string replace -r " .*\$" "" $__forgit_commands)
-
-# # no filename completion
-# complete -c forgit -x
-# complete -c forgit -l setup-alias -d "Setup forgit as git alias"
-# complete -c forgit -l cleanup-alias -d "Clean forgit alias from git config"
-
-# for cmd in $__forgit_commands
-#     set -l try_split (string split ' ' $cmd)
-#     if test (count $try_split) -gt 1
-#         # is a subcommand
-#         complete -c forgit -n "__fish_forgit_using_command $try_split[1]" -xa "$try_split[2]"
-#         set -l num_subcmds (echo $__forgit_commands_first_tokens_only | grep -o -i $try_split[1] | wc -l)
-#         complete -c forgit -n '__fish_forgit_needs_command' -xa $try_split[1] -d "Subcommands: $num_subcmds"
-#     else
-#         complete -c forgit -n '__fish_forgit_needs_command' -xa "$try_split[1]"
-#     end
-# end
